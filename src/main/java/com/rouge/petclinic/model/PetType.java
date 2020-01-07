97ff7c5e93739ce0e1ea8732cf8841fcef247dc3
rohan <rohanrawa09@gmail.com>
2020-01-07 17:49:58 +0530

    Pet Clinic Model Changes Foe Issue Number 1 . Closes #2

---
+++ src/main/java/com/rouge/petclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package com.rouge.petclinic.model;
+
+/**
+ * Created By Rohan Rawal On 07/01/2020
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/rouge/petclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.rouge.petclinic.model;
+
+/**
+ * Created By Rohan Rawal On 07/01/2020
+ */
+public class Vet extends Person{
+}
