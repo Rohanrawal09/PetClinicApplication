76747568a915664427c70b2995df0f425b253db2
rohan <rohanrawa09@gmail.com>
2020-01-09 14:35:44 +0530

    Added Pet Clinic Index Page And Controller With  Displaying List Of Owner From Bootstrap Data. Closes #8

--- pet-clinic-web/src/main/java/com/rouge/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/rouge/petclinic/controller/OwnerController.java
@@ -1,6 +1,9 @@
 package com.rouge.petclinic.controller;
 
+import com.rouge.petclinic.model.Owner;
+import com.rouge.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owner","/owner/index","/owner/index.html"})
-    public String callIndex(){
-        return "/owner/index";
+    public String callIndex(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
+        for (Owner owner : ownerService.findAll()){
+            System.out.println(owner.getId() +" ::::"+ owner.getFirstName() +" ::::"+ owner.getLastName());
+        }
+        return "owner/index";
+
     }
 }
