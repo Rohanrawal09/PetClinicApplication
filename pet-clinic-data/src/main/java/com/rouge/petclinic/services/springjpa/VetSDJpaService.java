b1a7ef23bb10c05679c33ab85b9d57705037fc3c
rohan <rohanrawa09@gmail.com>
2020-01-10 22:18:48 +0530

    Added Jpa Properties To All Entity Classes

---
+++ pet-clinic-data/src/main/java/com/rouge/petclinic/services/springjpa/VetSDJpaService.java
@@ -0,0 +1,51 @@
+package com.rouge.petclinic.services.springjpa;
+
+import com.rouge.petclinic.model.Vet;
+import com.rouge.petclinic.repositories.VetRepositories;
+import com.rouge.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created By Rohan Rawal On 1/10/2020
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepositories vetRepositories;
+
+    public VetSDJpaService(VetRepositories vetRepositories) {
+        this.vetRepositories = vetRepositories;
+    }
+
+    @Override
+    public Vet findById(Long aLong){
+        return vetRepositories.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepositories.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+       Set<Vet> vets = new HashSet<>();
+       vetRepositories.findAll().forEach(vets::add);
+       return vets;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepositories.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepositories.deleteById(aLong);
+    }
+}
